7b4697d4d9de9e81233b7144185cf0a676f3cbcf
codeskavy <kanikaverma0510@gmail.com>
2025-07-13 18:29:23 +0530

    fixing cors issue

--- chatapp-server-master/constants/config.js
+++ chatapp-server-master/constants/config.js
@@ -2,7 +2,8 @@ const corsOptions = {
   origin: [
     "http://localhost:5173",
     "http://localhost:4173",
-    process.env.CLIENT_URL,
+    "https://chatting-website-eight.vercel.app", // ✅ Add this
+    process.env.CLIENT_URL, // ✅ In case you use env vars too
   ],
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true,
